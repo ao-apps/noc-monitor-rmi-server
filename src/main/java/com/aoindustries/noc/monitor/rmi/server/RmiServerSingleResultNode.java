b776dc1648a87bd75abd26b59c73648e999286e3
AO Industries, Inc <support@aoindustries.com>
2022-04-16 02:03:23 -0500

    Added blank line before package declarations
    Resolves "EmptyLineSeparator: 'package' should be separated from previous line."

--- src/main/java/com/aoindustries/noc/monitor/rmi/server/RmiServerSingleResultNode.java
+++ src/main/java/com/aoindustries/noc/monitor/rmi/server/RmiServerSingleResultNode.java
@@ -1,6 +1,6 @@
 /*
  * noc-monitor-rmi-server - RMI Server for Network Operations Center Monitoring.
- * Copyright (C) 2012, 2020  AO Industries, Inc.
+ * Copyright (C) 2012, 2020, 2022  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -20,6 +20,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with noc-monitor-rmi-server.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 package com.aoindustries.noc.monitor.rmi.server;
 
 import com.aoindustries.noc.monitor.common.SingleResultNode;
